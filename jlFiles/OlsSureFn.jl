cfa8fa55aef4439a893b9a2f522535563750cdfb
Paul Söderlind <PaulSoderlind@users.noreply.github.com>
2019-05-09 16:19:28 +0200

    X,Y notation

--- jlFiles/OlsSureFn.jl
+++ jlFiles/OlsSureFn.jl
@@ -1,39 +1,39 @@
 """
-    OlsSureFn(y,x,m=0)
+    OlsSureFn(Y,X,m=0)
 
-LS of y on x; for one n dependent variables, same regressors
+LS of Y on X; for one n dependent variables, same regressors
 
 # Usage
-(b,res,yhat,Covb,R2a) = OlsSureFn(y,x,m)
+(b,res,Yhat,Covb,R2a) = OlsSureFn(Y,X,m)
 
 # Input
-- `y::Array`:     Txn, the n dependent variables
-- `x::Array`:     Txk matrix of regressors (including deterministic ones)
+- `Y::Array`:     Txn, the n dependent variables
+- `X::Array`:     Txk matrix of regressors (including deterministic ones)
 - `m::Int`:       scalar, bandwidth in Newey-West
 
 # Output
 - `b::Array`:     n*kx1, regression coefficients
-- `u::Array`:     Txn, residuals y - yhat
-- `yhat::Array`:  Txn, fitted values x*b
+- `u::Array`:     Txn, residuals Y - Yhat
+- `Yhat::Array`:  Txn, fitted values X*b
 - `V::Array`:     matrix, covariance matrix of vec(b)
 - `R2a::Number`:  n vector, R2 value
 
 """
-function OlsSureFn(y,x,m=0)
-    (T,n) = (size(y,1),size(y,2))
-    k     = size(x,2)
-    b     = x\y
-    yhat  = x*b
-    u     = y - yhat
+function OlsSureFn(Y,X,m=0)
+    (T,n) = (size(Y,1),size(Y,2))
+    k     = size(X,2)
+    b     = X\Y
+    Yhat  = X*b
+    u     = Y - Yhat
     g     = zeros(T,n*k)
     for i = 1:n
       vv      = (1+(i-1)*k):(i*k)   #1:k,(1+k):2k,...
-      g[:,vv] = x.*u[:,i]           #moment conditions for y[:,i] regression
+      g[:,vv] = X.*u[:,i]           #moment conditions for Y[:,i] regression
     end
     S0    = NWFn(g,m)            #Newey-West covariance matrix
-    Sxxi  = -x'x/T
+    Sxxi  = -X'X/T
     Sxx_1 = kron(Matrix(1.0I,n,n),inv(Sxxi))    #Matrix(1.0I,n,n) is identity matrix(n)
     V     = Sxx_1 * S0 * Sxx_1/T
-    R2a   = 1.0 .- var(u,dims=1)./var(y,dims=1)  #0.7 syntax
-    return b,u,yhat,V,R2a
+    R2a   = 1.0 .- var(u,dims=1)./var(Y,dims=1)
+    return b,u,Yhat,V,R2a
 end
