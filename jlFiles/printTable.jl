2556ec28117973b193046fa272c8ce917f62e2ff
Soderlind <Paul.Soderlind@unisg.ch>
2020-03-07 09:51:56 +0100

    more flexible printing

--- jlFiles/printTable.jl
+++ jlFiles/printTable.jl
@@ -1,18 +1,18 @@
 """
     printTable([fh::IO],x,colNames=[],rowNames=[];
-               width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="")
+               width=10,prec=3,NoPrinting=false,StringFmt="",cell00="")
 
 Print formatted table with row names (1st column) column names (1st row), and data matrix (the rest).
 
 
 # Input
-- `fh::IO`:           (optional) file handle. If not supplied, prints to screen
-- `x::Array`:         string, date or array to print
-- `width::Int`:       (keyword) scalar, width of printed cells. [10]
-- `prec::Int`:        (keyword) scalar, precision of printed cells. [3]
-- `NoPrinting::Bool`: (keyword) bool, true: no printing, just return formatted string [false]
-- `hmtlQ::Bool`:      (keyword) bool, true: format as htmlQ <td>cells</td> [false]
-- `cell00::String`:   (keyword) string, for row 0, column 0
+- `fh::IO`:            (optional) file handle. If not supplied, prints to screen
+- `x::Array`:          string, date or array to print
+- `width::Int`:        (keyword) scalar, width of printed cells. [10]
+- `prec::Int`:         (keyword) scalar, precision of printed cells. [3]
+- `NoPrinting::Bool`:  (keyword) bool, true: no printing, just return formatted string [false]
+- `StringFmt::String`: (keyword) string, "", "html", "csv"
+- `cell00::String`:    (keyword) string, for row 0, column 0
 
 # Output
 - `str::String`:      (if NoPrinting) string, (otherwise nothing)
@@ -28,7 +28,7 @@ printTable(xA,colNames,["1";"4"],width=12,prec=2)
 
 """
 function printTable(fh::IO,x,colNames=[],rowNames=[];
-                    width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="")
+                    width=10,prec=3,NoPrinting=false,StringFmt="",cell00="")
 
   isempty(x) && return nothing                        #do nothing is isempty(x)
 
@@ -44,27 +44,35 @@ function printTable(fh::IO,x,colNames=[],rowNames=[];
   rNamesWidth = maximum([length(rowNames[i]) for i = 1:length(rowNames)])  #max length of rowNames
   rNamesWidth = max(rNamesWidth,length(cell00))
 
-  if htmlQ                                             #print column names
-    cNamesStr = string("<tr><th>",lpad(cell00,rNamesWidth),"</th>")
+  iob = IOBuffer()
+  if StringFmt == "html"                                             #print column names
+    write(iob,"<tr><th>",lpad(cell00,rNamesWidth),"</th>")
     for i = 1:n
-      cNamesStr = string(cNamesStr,"<th>",lpad(colNames[i],width),"</th>")
+      write(iob,"<th>",lpad(colNames[i],width),"</th>")
     end
-    cNamesStr = string(cNamesStr,"</tr>")
+    write(iob,"</tr>")
+  elseif StringFmt == "csv"
+    write(iob,lpad(string(cell00,","),rNamesWidth))
+    for i = 1:n-1
+      write(iob,lpad(colNames[i],width),",")
+    end
+    write(iob,lpad(colNames[n],width))       #no , at line end
   else
-    cNamesStr = lpad(cell00,rNamesWidth)                 #cell 0,0
+    write(iob,lpad(cell00,rNamesWidth))                  #cell 0,0
     for i = 1:n                                          #create string
-      cNamesStr = string(cNamesStr,lpad(colNames[i],width))
+      write(iob,lpad(colNames[i],width))
     end
   end
+  write(iob,"\n")
 
-  xStr  = printmat(fh,x,width=width,prec=prec,NoPrinting=true,htmlQ=htmlQ)   #body of table, one long string
+  xStr  = printmat(fh,x,width=width,prec=prec,NoPrinting=true,StringFmt=StringFmt)   #body of table, one long string
   xStrV = split(xStr,"\n")                       #vector of strings (one per row of x)
 
-  iob = IOBuffer()
-  write(iob,cNamesStr,"\n")
   for i = 1:m                           #loop over rows in x, print rowNames[i] and x[i,:]
-    if htmlQ
+    if StringFmt == "html"
       write(iob,"<tr><td><b>",rpad(rowNames[i],rNamesWidth),"</td></b>",xStrV[i],"</tr> \n")
+    elseif StringFmt == "csv"
+      write(iob,rpad(string(rowNames[i],","),rNamesWidth),xStrV[i],"\n")
     else
       write(iob,rpad(rowNames[i],rNamesWidth),xStrV[i],"\n")
     end
@@ -80,9 +88,9 @@ function printTable(fh::IO,x,colNames=[],rowNames=[];
 
 end
                         #when fh is not supplied: printing to screen
-printTable(x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="") =
+printTable(x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,StringFmt="",cell00="") =
 printTable(stdout::IO,x,colNames,rowNames,
-                      width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ,cell00=cell00)
+                      width=width,prec=prec,NoPrinting=NoPrinting,StringFmt=StringFmt,cell00=cell00)
 #------------------------------------------------------------------------------
 
 
@@ -92,10 +100,10 @@ printTable(stdout::IO,x,colNames,rowNames,
 
 Call on printTable2 twice: to print to screen and then to an open file (IOStream)
 """
-function printTable2(fh,x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,htmlQ=false,cell00="")
-  printTable(x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ,cell00=cell00)      #to screen
+function printTable2(fh,x,colNames=[],rowNames=[];width=10,prec=3,NoPrinting=false,StringFmt="",cell00="")
+  printTable(x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,StringFmt=StringFmt,cell00=cell00)      #to screen
   if isa(fh,IOStream) && isopen(fh)
-    printTable(fh,x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,htmlQ=htmlQ,cell00=cell00) #to file
+    printTable(fh,x,colNames,rowNames,width=width,prec=prec,NoPrinting=NoPrinting,StringFmt=StringFmt,cell00=cell00) #to file
   end
 end
 #------------------------------------------------------------------------------
