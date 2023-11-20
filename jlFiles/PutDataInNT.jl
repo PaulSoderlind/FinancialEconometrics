0a2c13985bcc59e8f65b38f4d8dcc96362ee8f6c
Paul Soderlind <Paul.Soederlind@gmail.com>
2023-11-20 17:31:51 +0100

    better documentation

--- jlFiles/PutDataInNT.jl
+++ jlFiles/PutDataInNT.jl
@@ -1,11 +1,13 @@
+
 """
     PutDataInNT(x,header)
 
-Creates a NamedTuple with, for instance, `X.a`, `X.b` and `X.c` where `x` is a matrix and `header = ["a" "b" "c"]`.
+Creates a NamedTuple with, for instance, `N.X`, `N.Y` and `N.Z` where `x` is a matrix
+and `header = ["X" "Y" "Z"]` or `["X","Y","Z"]`.
 
 """
 function PutDataInNT(x,header)
-    namesB = tuple(Symbol.(header)...)                            #a tuple (:a,:b,:b)
-    X      = NamedTuple{namesB}([x[:,i] for i=1:size(x,2)])       #NamedTuple with X.a, X.b and X.c
-    return X
+    namesB = tuple(Symbol.(header)...)                            #a tuple (:X,:Y,:Z)
+    N      = NamedTuple{namesB}([x[:,i] for i=1:size(x,2)])       #NamedTuple with N.X, N.Y and N.Z
+    return N
 end
