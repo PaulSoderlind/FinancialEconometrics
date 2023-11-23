6c82539592bb22ec2ee0bdcb7ed6880b5bc4cec5
Paul Soderlind <Paul.Soederlind@gmail.com>
2023-11-23 21:04:51 +0100

    example of handling missings/NaNs

--- jlFiles/Ols.jl
+++ jlFiles/Ols.jl
@@ -47,15 +47,15 @@ LS of Y on X; for one dependent variable, using Newey-West covariance matrix
 (b,u,Yhat,V,R²) = OlsNWFn(Y,X,m)
 
 # Input
-- `Y::Array`:     Tx1, the dependent variable
-- `X::Array`:     Txk matrix of regressors (including deterministic ones)
+- `Y::Vector`:    T-vector, the dependent variable
+- `X::Matrix`:    Txk matrix of regressors (including deterministic ones)
 - `m::Int`:       scalar, bandwidth in Newey-West
 
 # Output
-- `b::Array`:     kx1, regression coefficients
-- `u::Array`:     Tx1, residuals Y - Yhat
-- `Yhat::Vector`: Tx1, fitted values X*b
-- `V::Array`:     kxk matrix, covariance matrix of b
+- `b::Vector`:    k-vector, regression coefficients
+- `u::Vector`:    T-vector, residuals Y - Yhat
+- `Yhat::Vector`: T-vector, fitted values X*b
+- `V::Matrix`:    kxk matrix, covariance matrix of b
 - `R²::Number`:   scalar, R² value
 
 """
