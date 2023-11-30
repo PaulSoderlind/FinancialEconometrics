6f1ac8c2f34d5ee89cfc9e5f4db86e334b8b7639
Paul Soderlind <Paul.Soederlind@gmail.com>
2023-11-30 20:27:18 +0100

    simplified the IR functions

--- jlFiles/TimeSeriesFilter.jl
+++ jlFiles/TimeSeriesFilter.jl
@@ -1,61 +1,60 @@
 ##------------------------------------------------------------------------------
 """
-    ARMAFilter(x,rho=[],theta=[],theta0=1.0,z0=[])
+    ARMAFilter(ϵ,rho=[],θ=[],θ₀=1.0,y₀=[])
 
-Calculate ARMA(p,q) transformation of an input series x. Uses explicit loop (instead of
+Calculate ARMA(p,q) transformation of an input series ϵ. Uses explicit loop (instead of
 DSP.filt).
 
 
 # Input
-- `x::Vector`:          T-vector with an input series
-- `rho::Vector`:        (optional) p-vector of autoregression coefficients, could be []
-- `theta::Vector`:      (optional) q-vector of moving average coefficients (lag 1 to q), could be []
-- `theta0::Number`:     (optional) scalar, coefficient on x[t] in MA part, [1]
-- `z0::Vector`:         (optional) p-vector, initial values of z, eg. [z(-1);z(0)], []
+- `ϵ::Vector`:      T-vector with an input series
+- `rho::Vector`:    (optional) p-vector of autoregression coefficients, could be []
+- `θ::Vector`:      (optional) q-vector of moving average coefficients (lag 1 to q), could be []
+- `θ₀::Number`:     (optional) scalar, coefficient on ϵ[t] in MA part, [1]
+- `y₀::Vector`:     (optional) p-vector, initial values of y, eg. [y(-1);y(0)], default: zeros(p)
 
 # Output
-- `z::Vector`:          T-vector with output from the filter
+- `y::Vector`:      T-vector with output from the filter
 
 # Notice
 (a) The process is
-    z[t] = rho[1]*z[t-1] + ... + rho[p]*z[t-p] + y[t], where
-    y[t] = theta0*x[t]   +  theta[1]*x[t-1] + ... + theta[q]*x[t-q]
-(b) The initial values of y are assumed to be zero (or z0)
-(c) The (time) order of z0 is as for x
-(d) The initial values of x are assumed to be zero
-(e) To calculate impulse response functions, use x = [1;zeros(T-1,1)]
-(f) There are no initial values in a pure MA and the case of q > p
+    y[t] = rho[1]*y[t-1] + ... + rho[p]*y[t-p] + θ₀*ϵ[t]  +  θ[1]*ϵ[t-1] + ... + θ[q]*ϵ[t-q]
+(b) The initial values of ϵ are assumed to be zero
+(c) To calculate impulse response functions, use ϵ = [1;zeros(T-1,1)]
+(d) There are no initial values in a pure MA and the case of q > p
     is handled with padding with zeros (see the code below)
 
 """
-function ARMAFilter(x::Vector,rho=Float64[],theta=Float64[],theta0=1,z0=Float64[])
+function ARMAFilter(ϵ,rho=[],θ=[],θ₀=1,y₀=[])
 
-  (T,q,p) = (length(x),length(theta),length(rho))
-  r       = max(p,q)                         #r obs are needed for start-up
 
-  x  = [zeros(r);x]                          #pad with zeros, clumsy but easy
-  Tb = r + T
+  T     = length(ϵ)
+  (q,p) = (length(θ),length(rho))
+  r     = max(p,q)                         #r obs are needed for start-up
 
-  theta_ud = q > 0 ? reverse([theta0;theta]) : theta0  #flip upside down
-  rho_ud   = p > 1 ? reverse(rho)            : rho
+  if (p > 0) && isempty(y₀)                #default y₀
+    y₀ = zeros(p)
+  end
+  (length(y₀) != p) && error("length of y₀ must equal p")
 
-  Tz0 = size(z0,1)
-  if isempty(z0)                             #to store output in
-    z = zeros(Tb)
-  else
-    z = [zeros(r-p);z0;zeros(Tb-(r-p)-Tz0)]  #pad with zeros if q > p
-  end                                        #zeros(r-p) is empty if r==p
+  θ_ud = q > 0 ? reverse([θ₀;θ]) : θ₀  #flip upside down
+  ρ_ud = p > 1 ? reverse(rho)    : rho
+
+  ϵ  = [zeros(r);ϵ]                          #pad with zeros, clumsy but easy
+  Tb = length(ϵ)                             #effective sample with padding
+
+  y = r > p ? [zeros(r-p);y₀;zeros(T)] : [y₀;zeros(T)]      #to store output in
 
   for t = r+1:Tb
       if p == 0                              #pure MA
-        z[t] = dot(theta_ud,x[t-q:t])
+        y[t] = dot(θ_ud,ϵ[t-q:t])
       else                                   #AR or ARMA
-        z[t] = dot(rho_ud,z[t-p:t-1]) + dot(theta_ud,x[t-q:t])
+        y[t] = dot(ρ_ud,y[t-p:t-1]) + dot(θ_ud,ϵ[t-q:t])
     end
   end
-  z = z[r+1:Tb]                              #cut padded part
+  y = y[r+1:Tb]                              #cut padded part
 
-  return z
+  return y
 
 end
 ##------------------------------------------------------------------------------
@@ -63,31 +62,31 @@ end
 
 ##------------------------------------------------------------------------------
 """
-    VARFilter(x,A,z0)
+    VARFilter(ϵ,A,y₀)
 
-Create z Txn matrix from VAR model where z[t,:] = A1*z[t-1,:] +...+Ap*z[t-p,:]+ x[t,:].
+Create y Txn matrix from VAR model where y[t,:] = A1*y[t-1,:] +...+Ap*y[t-p,:] + ϵ[t,:]
 A is an nxnxp array with cat(A1,A2,...,dims=3)
-z0 is pxn initial values of z (for [t=-2;t=-1;t=0] for a VAR(3))
+y₀ is pxn initial values of y (for [t=-2;t=-1;t=0] for a VAR(3))
 """
-function VARFilter(x,A,z0)
+function VARFilter(ϵ,A,y₀)
 
     p     = size(A,3)            #lag order
-    (T,n) = (size(x,1),size(x,2))
-    T0    = size(z0,1)
-    (p != T0) && error("z0 must be $p x $n")
+    (T,n) = (size(ϵ,1),size(ϵ,2))
+    (p != size(y₀,1)) && error("y₀ must be $p x $n")
 
-    z     = [z0;zeros(T,n)]
-    x     = [zeros(p,n);x]
-    for t = p+1:size(z,1)        #loop over t
-        for s = 1:p              #loop over lags, z[t-1],z[t-2],...,z[t-p]
-            z[t,:] = z[t,:] + A[:,:,s]*z[t-s,:]  #works since z[t,:] is a vector
+    y     = [y₀;zeros(T,n)]
+    ϵ     = [zeros(p,n);ϵ]
+    Tb    = size(y,1)
+    for t = p+1:Tb               #loop over t
+        for s = 1:p              #loop over lags, y[t-1],y[t-2],...,y[t-p]
+            y[t,:] = y[t,:] + A[:,:,s]*y[t-s,:]
         end
-        z[t,:] = z[t,:] + x[t,:]
+        y[t,:] = y[t,:] + ϵ[t,:]
     end
-    #printmat(z)
-    z = z[p+1:end,:]                #cut padded part
+    #printmat(y)
+    y = y[p+1:end,:]                #cut padded part
 
-    return z
+    return y
 
 end
 ##------------------------------------------------------------------------------
